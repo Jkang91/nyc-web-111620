1b3077f6b5118a0dd1434ff244f6b09d38733a94
Ori Markowitz <ori.markowitz@gmail.com>
2020-12-07 15:14:20 -0500

    Completed first project

--- goodburger/lib/models/application.rb
+++ goodburger/lib/models/application.rb
@@ -9,14 +9,14 @@ class Application
     def welcome
         ascii_helper("GoodBurger")
         sleep 2
-        puts "Welcome to GoodBurger, Home of the GoodBurger!"
+        puts Rainbow("Welcome to GoodBurger, Home of the GoodBurger!").orange
         sleep 3
     end
 
     def user_login_or_register
         system 'clear'
         ascii_helper("Welcome to GoodBurger")
-        prompt.select(Rainbow("Would you like to login or register?").red) do |menu|
+        prompt.select(Rainbow("Would you like to login or register?").orange) do |menu|
             menu.choice "Register", -> {register_sequence}
             menu.choice "Login", -> {login_sequence}
             menu.choice "Exit", -> {exit_app}
@@ -28,7 +28,7 @@ class Application
         ascii_helper("Login")
         user = User.login_a_user
         if user == nil
-            puts "Sorry, username or password was incorrect."
+            puts Rainbow("Sorry, username or password was incorrect.").orange
             prompt.select (Rainbow("Would you like to try again or register?").red) do |menu|
                 menu.choice "Try Again", -> {login_sequence}
                 menu.choice "Register", -> {register_sequence}
@@ -48,7 +48,7 @@ class Application
 
    def exit_app
     system 'clear'
-    puts "Thanks for stopping by GoodBurger! See you next time!"
+    puts Rainbow("Thanks for stopping by GoodBurger! See you next time!").orange
     ascii_helper("GoodBurger")
    end
 
@@ -56,7 +56,7 @@ class Application
     # user.reload
     system 'clear'
     ascii_helper("Welcome to GoodBurger")
-    prompt.select(Rainbow("Welcome, #{user.username}. What would you like to do?").red) do |menu|
+    prompt.select (Rainbow("Welcome, ").orange + Rainbow("#{user.username}.").red + Rainbow(" What would you like to do?").orange) do |menu|
         menu.choice "View Food Menu", -> {view_all_food}
         menu.choice "Add Food Item to Current Order", -> {add_food}
         menu.choice "View Current Order", -> {display_current_order}
@@ -71,7 +71,8 @@ class Application
     def view_all_food
         system 'clear'
         ascii_helper("GoodBurger Menu")
-        Food.all.each {|food| puts "#{food.name} - $#{food.price} - #{food.calories} cals"}
+        Food.all.each {|food| puts Rainbow("#{food.name}").orange + " - " + Rainbow("$#{food.price}").red + " - " + Rainbow("#{food.calories} cals").orange}
+        # Food.all.each {|food| puts "#{food.name} - $#{food.price} - #{food.calories} cals"}
         prompt.select("") do |menu|
             menu.choice "Main Menu", -> {main_menu}
         end
@@ -80,10 +81,10 @@ class Application
     def add_food
         system 'clear'
         ascii_helper("Welcome to GoodBurger")
-        prompt.select(Rainbow("Welcome to GoodBurger, Home of the GoodBurger, Can I Take Your Order?").red) do |menu|
+        prompt.select(Rainbow("Welcome to GoodBurger, Home of the GoodBurger, Can I Take Your Order?").orange) do |menu|
             menu.choice "Main Menu", -> {main_menu}
             Food.all.select do |food|
-                menu.choice "#{food.name} - $#{food.price}", -> {add_food_sequence(food)}
+                menu.choice Rainbow("#{food.name} - ").orange + Rainbow("$#{food.price}").red, -> {add_food_sequence(food)}
             end
             menu.choice "Main Menu", -> {main_menu}
         end
@@ -91,7 +92,7 @@ class Application
 
     def add_food_sequence(food)
         user.add_food_to_current_order(food)
-        puts "You've added #{food.name} to your order."
+        puts Rainbow("You've added ").orange + Rainbow("#{food.name}").red + Rainbow(" to your order.").orange
         sleep 2
         add_food
     end
@@ -100,8 +101,8 @@ class Application
         system 'clear'
         ascii_helper("#{user.username}'s' Current Order")  
         if user.current_order.foods == []
-            puts "You have zero items in your current order." 
-            puts "Please select 'Add Food Item to Current Order' to add items to your order."
+            puts Rainbow("You have zero items in your current order.").orange
+            puts Rainbow("Please select ").orange + Rainbow("Add Food Item to Current Order").red + Rainbow(" to add items to your order.").orange
             prompt.select (" ") do |menu|
                 menu.choice "Main Menu", -> {main_menu}
             end
@@ -109,15 +110,15 @@ class Application
             user.food_current_order
             if user.rewards_member || user.past_orders.length >= 10
                 user.rewards_member = true
-                puts "Congrats! As a GoodBurger Rewards Member, you get 10% off all orders."
-                puts "Your total amount is $#{user.current_order.total_price_with_discount}."
-                prompt.select (Rainbow("Would you like to place your order?").red) do |menu|
+                puts Rainbow("Congrats! As a GoodBurger Rewards Member, you get ").orange + Rainbow("10% off all orders.").red
+                puts Rainbow("Your total amount is ").orange + Rainbow("$#{user.current_order.total_price_with_discount}.").red
+                prompt.select (Rainbow("Would you like to place your order?").orange) do |menu|
                     menu.choice "Yes", -> {purchasing_order}
                     menu.choice "No", -> {main_menu}
                 end
             else
-                puts "Your total amount is $#{user.current_order.total_price}."
-                prompt.select (Rainbow("Would you like to place your order?").red) do |menu|
+                puts Rainbow("Your total amount is ").orange + Rainbow("$#{user.current_order.total_price}.").red
+                prompt.select (Rainbow("Would you like to place your order?").orange) do |menu|
                     menu.choice "Yes", -> {purchasing_order}
                     menu.choice "No", -> {main_menu}
                 end
@@ -129,7 +130,7 @@ class Application
         system 'clear'
         ascii_helper("Order Confirmed")
         user.purchase_current_order
-        puts "Your order has been purchased! Enjoy!"
+        puts Rainbow("Your order has been purchased! Enjoy!").orange
         sleep 5
         main_menu
     end
@@ -137,7 +138,7 @@ class Application
     def cancel_order
         system 'clear'
         ascii_helper("Cancel Order")
-        prompt.select (Rainbow("What would you like to cancel?").red) do |menu|
+        prompt.select (Rainbow("What would you like to cancel?").orange) do |menu|
             menu.choice "Cancel entire order", -> {cancel_all_order}
             menu.choice "Remove item from order", -> {remove_item_sequence}
             menu.choice "Main Menu", -> {main_menu}
@@ -147,11 +148,11 @@ class Application
     def cancel_all_order
         system 'clear'
         if user.current_order.food_orders.length == 0
-            puts "You do not have a current order to cancel!"
+            puts Rainbow("You do not have a current order to cancel!").orange
             sleep 5
         else
             user.cancel_current_order
-            puts "Your order is now cancelled."
+            puts Rainbow("Your order is now cancelled.").orange
             sleep 5 
         end
         main_menu
@@ -160,27 +161,27 @@ class Application
     def remove_item_sequence
         system 'clear'
         if user.current_order.food_orders.length == 0
-            puts "You have no items to remove!"
+            puts Rainbow("You have no items to remove!").orange
             sleep 5
         else
-            prompt.select (Rainbow("Which food item would you like to remove?").red) do |menu|
+            prompt.select (Rainbow("Which food item would you like to remove?").orange) do |menu|
                 user.current_order.food_orders.select do |food_order|
-                    menu.choice "#{food_order.food.name}", -> {user.remove_food_from_current_order(food_order.id)}
+                    menu.choice Rainbow("#{food_order.food.name}").orange, -> {user.remove_food_from_current_order(food_order.id)}
                 end
                 menu.choice "Main Menu", -> {main_menu}
             end
-            puts "Your item has been removed."
+            puts Rainbow("Your item has been removed.").red
             sleep 2
         end
         main_menu
     end
 
     def user_stat_message
-        puts "Your favorite food is #{user.favorite_food}!"
+        puts Rainbow("Your favorite food is ").orange + Rainbow("#{user.favorite_food}!").red
         puts "-------"
-        puts "You have eaten #{user.total_calories_consumed_ever} total calories at GoodBurger! Congrats, Fatty!"
+        puts Rainbow("You have eaten ").orange + Rainbow("#{user.total_calories_consumed_ever}").red + Rainbow(" total calories at GoodBurger! Congrats, Fatty!").orange
         puts "-------"
-        puts "I can't believe you've spent $#{user.total_amount_spent_ever.round(2)} at GoodBurger all-time! Better take out a loan!"
+        puts Rainbow("I can't believe you've spent ").orange + Rainbow("$#{user.total_amount_spent_ever.round(2)}").red + Rainbow(" at GoodBurger all-time! Better take out a loan!").orange
         puts "-------"
     end
 
@@ -188,9 +189,9 @@ class Application
         system 'clear'
         ascii_helper("#{user.username}'s Stats")
         if user.past_orders.length == 0
-            puts "Place an order to start seeing your Good Burger stats!"
+            puts Rainbow("Place an order to start seeing your Good Burger stats!").orange
         elsif user.rewards_member || user.past_orders.length > 10
-            puts "You are a GoodBurger Rewards Member! Enjoy your 10% off all orders!"
+            puts Rainbow("You are a GoodBurger Rewards Member! Enjoy your ").orange + Rainbow("10% off all orders!").red
             puts "-------"
             user_stat_message
         else
@@ -207,14 +208,14 @@ class Application
         system 'clear'
         ascii_helper("#{user.username}'s Order History")
         if user.past_orders.length == 0
-            puts "You've never ordered before!"
+            puts Rainbow("You've never ordered before!").orange
         else
             user.past_orders.each_with_index do |order, index|
-                puts "Order: #{index + 1}"
+                puts Rainbow("Order: #{index + 1}").orange
                 order.food_orders.each do |food_order|
-                    puts "#{food_order.food.name} - $#{food_order.food.price}"
+                    puts Rainbow("#{food_order.food.name} - $#{food_order.food.price}").red
                 end
-                puts "Total: $#{order.order_total}"
+                puts Rainbow("Total: $#{order.order_total}").orange
                 puts "----------"
             end
         end
